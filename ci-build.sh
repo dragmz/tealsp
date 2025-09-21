da0a96e83dc2c748c4c2df5432794768bc8a17bb
Marcin Zawiejski <dragmz@gmail.com>
2025-09-22 01:24:20 +0200

    build

---
+++ ci-build.sh
@@ -0,0 +1,20 @@
+pwd
+ls -la ~
+
+wget -q https://go.dev/dl/go1.23.4.windows-amd64.zip
+unzip -q go1.23.4.windows-amd64.zip
+
+export PATH=$PATH:$(pwd)/go/bin
+export GOROOT=$(pwd)/go
+
+go version
+git submodule init
+git submodule update
+
+cd go-algorand
+./scripts/configure_dev.sh
+make install
+
+cp ~/go/bin/tealsp.exe ../tealsp.exe
+
+ls -la
